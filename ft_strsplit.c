69573795eb505073f0a7081a0f803f3224b215db
Cameron Dimitroff <cdimitro@e1z2r10p6.42.us.org>
2019-03-11 22:41:27 -0700

    h

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,7 +6,7 @@
 /*   By: cdimitro <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/23 16:43:24 by cdimitro          #+#    #+#             */
-/*   Updated: 2019/03/11 22:26:27 by cdimitro         ###   ########.fr       */
+/*   Updated: 2019/03/11 22:41:02 by cdimitro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,17 @@ char	**ft_strsplit(const char *str, char c)
 {
 	int		i;
 	int		j;
-	int		wrd;
+	int		k;
 	char	**res;
 
 	i = 0;
 	j = 0;
-	wrd = ft_word_count(str, c);
+	k = ft_word_count(str, c);
 	if (!(res = (char **)malloc(sizeof(res) * (ft_word_count(str, c) + 2))))
 		return (NULL);
 	while (str[i] == c && str[i])
 		i++;
-	while (j < wrd && str[i])
+	while (j < k && str[i])
 	{
 		res[j] = ft_word_delim(str, c, &i);
 		j++;
